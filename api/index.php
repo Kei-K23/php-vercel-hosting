c2ad58a8b2a29fb4f2e1a0e63b02324489d82235
Kei-K23 <keiksl2301@gmail.com>
2024-06-08 10:51:26 +0630

    Update vercel.json file 10

--- api/index.php
+++ api/index.php
@@ -1,21 +1,4 @@
 <?php
-phpinfo()
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-
-<body>
-    <h1>PHP + Vercel 🚀</h1>
-    <p>Your are viewing Home page</p>
-    <a href="./about.php">Go to About page</a>
-</body>
-
-</html>
+header('content-type: application/json');
+echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
